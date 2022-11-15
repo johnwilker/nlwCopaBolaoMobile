ef0a1f58e912ee50b08d9d2991c5da3dd0a47b1d
John Wilker <johnwilker2@gmail.com>
2022-11-15 10:35:09 -0300

    nlwCopaBolaoMobile finished

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
+// const { inlineStyles } = require( "react-native-svg" );
+
 module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: ['inline-dotenv']
   };
 };
